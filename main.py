c00201eafb4f7c641e32311ea9dcc0a24cbb3482
priyantkushwaha665-png <priyantkushwaha665@gmail.com>
2026-05-14 12:26:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -7,12 +8,12 @@ DB_FILE = 'database.db'
 def init_db():
     conn = sqlite3.connect(DB_FILE)
     cur = conn.cursor()
-    # Products Table
+    # IMPORTANT: Ye purani table delete karega taaki naye columns ban sakein
+    cur.execute('DROP TABLE IF EXISTS products')
     cur.execute('''CREATE TABLE IF NOT EXISTS products 
                    (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, real_price TEXT, 
                     discount_price TEXT, img_url TEXT, buy_link TEXT, category TEXT, 
                     wrx_review TEXT, rating INTEGER)''')
-    # User Reviews Table
     cur.execute('''CREATE TABLE IF NOT EXISTS user_reviews 
                    (id INTEGER PRIMARY KEY AUTOINCREMENT, product_id INTEGER, 
                     user_name TEXT, user_text TEXT)''')
@@ -23,18 +24,13 @@ init_db()
 
 @app.route('/')
 def index():
-    cat = request.args.get('cat')
     conn = sqlite3.connect(DB_FILE)
     cur = conn.cursor()
-    if cat:
-        cur.execute("SELECT * FROM products WHERE category = ?", (cat,))
-    else:
-        cur.execute("SELECT * FROM products")
+    cur.execute("SELECT * FROM products")
     products = cur.fetchall()
     conn.close()
     return render_template('index.html', products=products)
 
-# --- NAYA PRODUCT PAGE ROUTE ---
 @app.route('/product/<int:id>')
 def product_page(id):
     conn = sqlite3.connect(DB_FILE)
@@ -44,9 +40,11 @@ def product_page(id):
     cur.execute("SELECT * FROM user_reviews WHERE product_id = ?", (id,))
     reviews = cur.fetchall()
     conn.close()
+    if product is None:
+        return "Product Not Found", 404
     return render_template('product.html', product=product, reviews=reviews)
 
-@app.route('/submit_review', method=['POST'])
+@app.route('/submit_review', methods=['POST'])
 def submit_review():
     p_id = request.form['product_id']
     name = request.form['user_name']
@@ -59,4 +57,5 @@ def submit_review():
     return redirect(f'/product/{p_id}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
